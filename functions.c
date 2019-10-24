d40b113eaeb8d70e56eb5237ca98ede3310e2291
Ivan Lin <ilin00@stuy.edu>
2019-10-23 21:27:07 -0400

    v1

---
+++ functions.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "functions.h"
+
+void print_list(struct node *n ){
+	 // struct node *first_node = n;
+	 // *int s = malloc(100 * sizeof(int));
+	 // int counter = 0;
+	 // while (n->next != first_node){
+		 // s[counter] = n->i;
+		 // n = n->next;
+		 // counter += 1;
+	 // }
+	 //printf("[%d]",s);
+	 printf("0");
+}
