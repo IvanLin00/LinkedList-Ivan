6c3bf0f5291949b3418edfd334bfb263d71a489f
Ivan Lin <ilin00@stuy.edu>
2019-10-24 19:38:42 -0400

    all except for remove

--- functions.c
+++ functions.c
@@ -3,14 +3,50 @@
 #include "header.h"
 
 void print_list(struct node *n ){
-	 // struct node *first_node = n;
-	 // *int s = malloc(100 * sizeof(int));
-	 // int counter = 0;
-	 // while (n->next != first_node){
-		 // s[counter] = n->i;
-		 // n = n->next;
-		 // counter += 1;
-	 // }
-	 //printf("[%d]",s);
-	 printf("0");
+	 printf("[");
+	 while(n != NULL){
+		 if(n->next == NULL){
+			 printf("%d",n->i);
+			 break;
+		 }
+		 printf("%d,",n->i);
+		 n = n->next;
+	 }
+	 printf("]\n");
 }
+
+struct node * insert_front(struct node *n, int data){
+	 struct node *n1 = malloc(sizeof(struct node));
+	 n1 -> i = data;
+	 n1 -> next = n;
+	 return n1;
+}
+
+struct node * free_list(struct node *n){
+	 struct node *freeNode = n;
+	 n = n->next;
+	 freeNode = NULL;
+	 free(freeNode);
+	 return n;
+}
+
+//struct node * remove(struct node *front, int data){ // currently has seg fault runtime error
+	 // struct node *prev;
+	 // struct node *n1 = malloc(sizeof(struct node));
+	 // n1 = NULL;
+	 // struct node *first = front;
+	 // while(front != NULL){
+		 // if (front-> i == data){
+			 // n1 = front;
+			 // break;
+		 // }
+		 // prev = front;
+		 // front = front-> next;
+	 // }
+	 // if (n1 != NULL){
+		// prev->next = n1 -> next;
+		// free_list(n1);
+	 // }
+	 // return front; 
+	 
+//}
--- main.c
+++ main.c
@@ -1,12 +1,30 @@
-#include "header.h"
+//#include "header.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+struct node {int i; struct node *next;};
 
 int main(){
-	 //struct node n1;
-	 //n1 -> i = 0;
-	 //struct node n2;
-	 //n1 -> next = n2l
-	// n2 -> i = 1;
-	 //n2 -> next = n1;
+	 struct node *n1= malloc(sizeof(struct node));
+	 n1 = NULL;
+	 printf("Printing empty list:\n");
+	 print_list(n1);
+	 printf("Adding #s 0-9 to list.\n");
+	 for (int i = 0; i<10; i++){
+		 n1=insert_front(n1,i);
+	 }
+	 print_list(n1);
+	 printf("Freeing list.\n");
+	 
+	 //printf("Removing node with data 4.\n");
+	 //n1 = remove(n1,4);
 	 //print_list(n1);
+	 
+	 for (int i = 0; i<10; i++){
+		 printf("Freeing node: %d\n",n1-> i);
+		 n1 = free_list(n1);
+	 }
+	 printf("Printing list:\n");
+	 print_list(n1);
 	 return 0;
 }
